4dccde7b08e5582b8e2aba30f2b7e7279b2adba0
Ashtell <mhallisawesome@gmail.com>
2017-03-31 15:59:39 -0600

    ability to navagate to programming topics

--- script/navagate.js
+++ script/navagate.js
@@ -1,4 +1,19 @@
-function moveToPage(week, day) {
+function moveToDate(week, day) {
     var command = "$('#content').load('mainContent/week" + week + "/day" + day + ".html')";
+    eval(command);
+}
+function moveToTopic(topic, page) {
+    if(topic == 1){ //Javascript
+        topic = "javascript";
+    }else if(topic == 2){ //html
+        topic = "html";
+    }else if(topic == 3){ //css
+        topic = "css";
+    }else if(topic == 4){ //python
+        topic = "python";
+    }else if(topic == 5){ //c++
+        topic = "cpp";
+    }
+    var command = "$'#content').load('topics/"+topic+"/page"+page+".html')";
     eval(command);
 }
